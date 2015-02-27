980aa87586ced82c0716c85cc1159c10936731a8
Kirill Liubun <kreol_123@ukr.net>
2015-02-27 19:57:26 +0200

    Initial commit

---
+++ src/com/gym/TestFile.java
@@ -0,0 +1,18 @@
+package com.gym;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+/**
+ * Created by Nout on 27/02/2015.
+ */
+public class TestFile extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
